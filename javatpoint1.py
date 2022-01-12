4bb763821b06e015f17a3d6d3af7d614756bf452
tharunkumar24 <96691548+tharunkumar24@users.noreply.github.com>
2022-01-12 20:03:33 +0530

    Add files via upload

---
+++ javatpoint1.py
@@ -0,0 +1,6 @@
+sptr=int(input('speed of train in kmph:'))
+sptr=sptr*(5/18)
+print('speed of train in m/s is: ',sptr)
+tt=int(input('time taken by train to cross the pole:'))
+lot=sptr*tt
+print('length of tain in meters is:',lot)
